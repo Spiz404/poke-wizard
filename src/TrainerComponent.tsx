22a5c9509de6ce9d8f8eb482eb54d31f1205407e
spiz404 <lorespiz03@gmail.com>
2024-09-12 13:20:39 +0200

    refactor

--- src/TrainerComponent.tsx
+++ src/TrainerComponent.tsx
@@ -1,4 +1,4 @@
-import {TextField, FormControl, InputLabel, Input, FormHelperText } from "@mui/material"
+import {TextField } from "@mui/material"
 import { useState, useEffect } from "react"
 
 interface TrainerDetails {
