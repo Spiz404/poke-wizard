bf373534631333820d576f34ad42521d433077af
spiz404 <lorespiz03@gmail.com>
2024-09-13 19:46:46 +0200

    done dialog types

--- src/PokemonComponent.tsx
+++ src/PokemonComponent.tsx
@@ -1,42 +1,46 @@
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Chip } from '@mui/material'
+import { Dialog , DialogContent, Button, Chip } from '@mui/material'
 import { PokemonAPIResponse } from './types/pokemonAPI'
+import {Type, Species, Move } from './types/pokemonAPI'
 
-const PokemonComponent = ({pokemon, open, onClose, selectPokemon}: {pokemon: PokemonAPIResponse, open: boolean, onClose: () => void, selectPokemon: (pokemon: any) => void}) => {
+const PokemonComponent = ({pokemon, open, onClose, selectPokemon}: {pokemon: PokemonAPIResponse | null, open: boolean, onClose: () => void, selectPokemon: (pokemon: any) => void}) => {
 
-    return (
-        <Dialog open={open}>
-            
-            <DialogContent style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <h1>{pokemon.name}</h1>
-                <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                    <img src={pokemon && pokemon.sprites.front_default} alt={pokemon.name} />
-                    <img src={pokemon && pokemon.sprites.back_default} alt={pokemon.name} />
-                </div>
-                <h2>Types</h2>
-                <div className="types-container">
-                    {pokemon.types.map((type: any) => {
-                        return <Chip key={type.type.name} label={type.type.name}></Chip>
-                    })}
-                </div>
-                <h2>Forms</h2>
-                <div className="forms-container">
-                    {pokemon.forms.map((form: any) => {
-                        return <Chip key={form.name} label={form.name}></Chip>
-                    })}
-                </div>
-                <h2>Moves</h2>
-                <div className="moves-container">
-                    {pokemon.moves.map((move: any) => {
-                        return <Chip key={move.name} label={move.move.name}></Chip>
-                    })}
-                </div>
-                <div className="dialog-buttons-container">
-                    <Button variant="contained" color="primary" onClick={() => {selectPokemon(pokemon); onClose()}}>Select Pokemon</Button>
-                    <Button variant="contained" color="error" onClick={onClose}>Close</Button>
-                </div>
-            </DialogContent>
-        </Dialog>
-    )
+    if (pokemon) {
+
+        return (
+            <Dialog open={open}>
+                
+                <DialogContent style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    <h1>{pokemon.name}</h1>
+                    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                        <img src={pokemon && pokemon.sprites.front_default} alt={pokemon.name} />
+                        <img src={pokemon && pokemon.sprites.back_default} alt={pokemon.name} />
+                    </div>
+                    <h2>Types</h2>
+                    <div className="types-container">
+                        {pokemon.types.map((type: Type) => {
+                            return <Chip key={type.type.name} label={type.type.name}></Chip>
+                        })}
+                    </div>
+                    <h2>Forms</h2>
+                    <div className="forms-container">
+                        {pokemon.forms.map((form: Species) => {
+                            return <Chip key={form.name} label={form.name}></Chip>
+                        })}
+                    </div>
+                    <h2>Moves</h2>
+                    <div className="moves-container">
+                        {pokemon.moves.map((move: Move) => {
+                            return <Chip key={move.move.name} label={move.move.name}></Chip>
+                        })}
+                    </div>
+                    <div className="dialog-buttons-container">
+                        <Button variant="contained" color="primary" onClick={() => {selectPokemon(pokemon); onClose()}}>Select Pokemon</Button>
+                        <Button variant="contained" color="error" onClick={onClose}>Close</Button>
+                    </div>
+                </DialogContent>
+            </Dialog>
+        )
+    }
 }
 
 export default PokemonComponent;
